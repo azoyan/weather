b6d08e71cd9f0f39c7ab214ad363c02fbd0491c3
Ivan Azoyan <azoyan.89@mail.ru>
2025-05-22 23:30:53 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'weather-app-cache-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
